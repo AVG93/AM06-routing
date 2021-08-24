d4579209663b1b59895308b1cf8d4e2ecc2d657f
AVG93 <arturovillgon@hotmail.com>
2021-08-24 15:10:42 -0500

    reactive forms

--- src/app/tres/tres.component.ts
+++ src/app/tres/tres.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-tres',
@@ -6,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tres.component.css']
 })
 export class TresComponent implements OnInit {
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.formGroup = new FormGroup({
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      correo: new FormControl('', [Validators.required, Validators.email])
+    });
   }
 
+  enviar() {
+    console.warn(JSON.stringify(this.formGroup.value));
+  }
+
+  get form() {
+    return this.formGroup.controls;
+  }
 }
